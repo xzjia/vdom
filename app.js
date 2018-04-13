9c5251e25c4b0c99e4d85cce4c9a6dba7efc19ae
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-13 10:23:55 +0900

    Write working solution for updateElement

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ function createVDOM(type, props = null, ...children) {
 
 // eslint-disable-next-line no-unused-vars
 function createElement(node) {
+  if (typeof node === "string") {
+    return document.createTextNode(node);
+  }
   const element = document.createElement(node.type);
   for (let prop in node.props) {
     if (prop === "class") {
@@ -26,7 +29,6 @@ function createElement(node) {
   return element;
 }
 
-// eslint-disable-next-line no-unused-vars
 function changed(node1, node2) {
   return (
     typeof node1 !== typeof node2 ||
@@ -51,82 +53,25 @@ function propsDiffer(node1, node2) {
 // parentNode!
 
 // eslint-disable-next-line no-unused-vars
-function updateElement(target, newNode, oldNode) {
-  if (changed(newNode, oldNode)) {
-    console.log("TYPE DIFFERENT!");
-    return;
-  } else if (propsDiffer(newNode, oldNode)) {
-    console.log("PROPS DIFFERENT!");
-    console.log({ target });
-    console.log({ newNode });
-    target.parentNode.insertBefore(createElement(newNode), target);
+function updateElement(target, newNode, oldNode, parent = target.parentNode) {
+  if (!oldNode) {
+    parent.appendChild(createElement(newNode));
+  } else if (!newNode) {
     target.remove();
-    return;
+  } else if (changed(newNode, oldNode) || propsDiffer(newNode, oldNode)) {
+    parent.replaceChild(createElement(newNode), target);
   } else {
-    const longerNode = Math.max(
+    const longerNodeLength = Math.max(
       newNode.children.length,
       oldNode.children.length
     );
-    for (let i = 0; i < longerNode; i++) {
+    for (let i = 0; i < longerNodeLength; i++) {
       updateElement(
-        target.children[i],
+        target.childNodes[i],
         newNode.children[i],
-        oldNode.children[i]
+        oldNode.children[i],
+        target
       );
     }
   }
-}
-
-// eslint-disable-next-line no-unused-vars
-function updateElementOLD(target, newNode, oldNode) {
-  function compareNodes(newChild, oldChild) {
-    if (changed(newChild, oldChild)) {
-      console.log({ newChild, oldChild });
-    }
-    if (oldChild.children.length < newChild.children.length) {
-      appendAddition(oldChild.children, newChild.children);
-      // else look for deletion
-      // else look for changed props
-    } else if (oldChild.children.length > newChild.children.length) {
-      removeDeletion(oldChild.children, newChild.children);
-    } else {
-      for (let i = 0; i < newChild.children.length; i++) {
-        compareNodes(newChild.children[i], oldChild.children[i]);
-      }
-    }
-  }
-
-  function removeDeletion(oldChildren, newChildren) {
-    for (let i = 0; i < oldChildren.length; i++) {
-      if (changed(oldChildren[i], newChildren[i])) {
-        if (newChildren[i] === undefined) {
-          target.appendChild(createElement(newChildren[i]));
-        } else {
-          target.insertBefore(
-            createElement(newChildren[i]),
-            target.children[i]
-          );
-          break;
-        }
-      }
-    }
-  }
-
-  function appendAddition(oldChildren, newChildren) {
-    for (let i = 0; i < newChildren.length; i++) {
-      if (changed(oldChildren[i], newChildren[i])) {
-        if (oldChildren[i] === undefined) {
-          target.appendChild(createElement(newChildren[i]));
-        } else {
-          target.insertBefore(
-            createElement(newChildren[i]),
-            target.children[i]
-          );
-          break;
-        }
-      }
-    }
-  }
-  compareNodes(newNode, oldNode);
-  const changes = {};
 }
